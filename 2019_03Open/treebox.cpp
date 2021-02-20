1b76d1e3ee649ec3cedeec31c8a9dddee045a211
Billy <billyz404meter@gmail.com>
2021-02-20 14:39:57 -0800

    treebox visual

--- 2019_03Open/treebox.cpp
+++ 2019_03Open/treebox.cpp
@@ -66,15 +66,53 @@ void notifyFJ(int a, int b){
     if(c != b) addBox(x[C],y[C],x[b],y[b]);
 }
 
-#if 0
+#if defined(DEMO)
 int N,Q;
 int getN() {return N;}
 int getQ() {return Q;}
+char g1[100][100],g2[100][100];
+void setup(){
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            g2[i][j] = g1[i][j];
+        }
+    }
+}
+void print(){
+    for(int i = 1; i <= N; i++){
+        cout << (g2[i]+1) << endl;
+    }   
+    cout << endl;
+}
+void init(){
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            g1[i][j] = ' ';
+        }
+    }
+}
+
+char conv(int i){
+    return (i < 10) ? ('0'+i) : ('A'+i-10);
+}
+
 void setFarmLocation(int ID, int X, int Y){
     cout << ID << ' ' << X << ' ' << Y << endl;
+    g1[X][Y] = conv(ID);
 }
 void addBox(int X1, int Y1, int X2, int Y2){
     cout << X1 << ' ' << Y1 << ' ' << X2 << ' ' << Y2 << endl;
+    int ax = min(X1,X2), bx = max(X1,X2);
+    int ay = min(Y1,Y2), by = max(Y1,Y2);
+    for(int i = ax; i <= bx; i++){
+        g2[i][ay]= g2[i][by] = '*';
+    }
+    for(int i = ay; i <= by; i++){
+        g2[ax][i]= g2[bx][i] = '*';
+    }
+
+    g2[X1][Y1] = '+';
+    g2[X2][Y2] = '-';
 }
 
 int main(){
@@ -85,11 +123,16 @@ int main(){
         cin >> a >> b;
         addRoad(a,b);
     }
+    init();
     buildFarms();
+    setup();
+    print();
     for(int i = 0; i < Q; i++){
         int a,b;
         cin >> a >> b;
+        setup();
         notifyFJ(a,b);
+        print();
     }
 }
 #endif
