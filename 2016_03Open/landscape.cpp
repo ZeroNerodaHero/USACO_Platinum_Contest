eb34b1ee55cf440cb03c1db5b4441fc06c49b5ba
Billy <billyz404meter@gmail.com>
2021-03-20 16:47:47 -0700

    Landscape no dependence on K

--- 2016_03Open/landscape.cpp
+++ 2016_03Open/landscape.cpp
@@ -5,6 +5,8 @@ const int MX = 100005;
 typedef long long ll;
 typedef pair<int, int> PI;
 typedef vector<int> VI;
+#define f first
+#define s second 
 
 void cio(string fname){
     ios_base::sync_with_stdio(0); cin.tie(0);
@@ -12,39 +14,42 @@ void cio(string fname){
     freopen((fname+".out").c_str(),"w",stdout);
 }
 
-int N,Z,T,u,v,a[MX];
+int N,Z,u,v,a;
 ll ans, cost[2];
-priority_queue<int> s[2];
+priority_queue<PI> s[2];
 
 int main(){
     cio("landscape");
     cin >> N >> cost[0] >> cost[1] >> Z;
-    T = cost[0] + cost[1]; 
-//cout << "cost 0 " << cost[0] << " - " << cost[1] << endl;
     for(int i = 1; i <= N; i++){
         cin >> u >> v;
-        a[i] = v - u;
-//cout << i << " : " << a[i] << endl;
-        if(a[i] == 0) continue;
-        if(a[i] < 0){
+        a = v - u;
+        if(a == 0) continue;
+        if(a < 0){
             u = 1;
-            a[i] = -a[i];
+            a = -a;
         } else{
             u = 0;
         }
         v = 1^u;
-        for(int j = 1; j <= a[i]; j++){
-            if(!s[v].empty() && ((i * Z - s[v].top()) < cost[u])){
-                int tmp = s[v].top();
-                if((i * Z - tmp) < cost[u]){
-                    ans += i * Z - tmp;
-                    s[v].pop();
-                    s[u].push(2* i * Z - tmp);
-                }
+        if(!s[v].empty() && ((i * Z - s[v].top().f) >= cost[u])) s[v].pop();
+        if(!s[u].empty() && ((i * Z - s[u].top().f) >= cost[v])) s[u].pop();
+        while(a > 0){
+            if(!s[v].empty() && ((i * Z - s[v].top().f) < cost[u])){
+                int tmp = s[v].top().f;
+                int tmps = s[v].top().s;
+                int newcost = (i * Z - tmp);
+                int t = min(a,tmps);
+                ans += newcost * t;
+                a -= t; tmps -= t;
+                s[v].pop();
+                if(tmps > 0) s[v].push(PI(tmp,tmps));
+                s[u].push(PI(i * Z + newcost,t));
             }
             else{
-                s[u].push(i*Z + cost[u]);
-                ans += cost[u];
+                s[u].push(PI(i*Z + cost[u],a));
+                ans += cost[u]*a;
+                a = 0;
             }
         }
     }   
